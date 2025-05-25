f18fbfe269cc3c0b61825caf02e5cca47371a769
Ivans Kravcevs <I.Kravcevs@student.tudelft.nl>
2025-05-25 13:27:00 +0200

    Added dvc pipeline with Excellent criteria

---
+++ training/modeling/preprocess.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from tqdm import tqdm
+from pathlib import Path
+from lib_ml.preprocessing import _clean
+from loguru import logger
+import typer
+from training.config import DATA_DIR, PROCESSED_DATA_DIR
+
+app = typer.Typer()
+
+@app.command()
+def preprocess(
+    dataset_path: Path = DATA_DIR / "raw" / "a1_RestaurantReviews_HistoricDump.tsv",
+    corpus_path: Path = PROCESSED_DATA_DIR / "corpus.pkl"
+):
+    logger.info(f"Loading dataset from {dataset_path}")
+    df = pd.read_csv(dataset_path, delimiter="\t", quoting=3)
+    
+    corpus = []
+    for i in tqdm(range(len(df)), desc="Cleaning text"):
+        corpus.append(_clean(df["Review"][i]))
+    logger.info("Preprocessing complete")
+
+    logger.info(f"Saving cleaned corpus to {corpus_path}")
+    corpus_path.parent.mkdir(parents=True, exist_ok=True)
+    pd.Series(corpus).to_pickle(corpus_path)
+
+if __name__ == "__main__":
+    app()

0d93cd2aaf5cabf67900630c18f17b43c4212358
Marvin Blommestijn <M.J.C.Blommestijn-1@student.tudelft.nl>
2025-06-25 21:30:09 +0200

    fixed linting

--- training/modeling/transform.py
+++ training/modeling/transform.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-import pickle
 
 from loguru import logger
 import pandas as pd
@@ -27,7 +26,7 @@ def transform(
 
     logger.info(f"Saving features to {dataset_path}")
     feature_names = tp._vectorizer.get_feature_names_out()
-    pd.DataFrame(X.toarray(), columns=feature_names).to_csv(dataset_path, index=False)
+    pd.DataFrame(X.toarray(), columns=tp.get_feature_names()).to_csv(dataset_path, index=False)
 
 
     # Labels: assumed to be in original corpus file name
